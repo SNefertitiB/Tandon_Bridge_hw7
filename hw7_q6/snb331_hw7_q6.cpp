ac4fb622af2f77d378ae22492e2676ac63c01e9b
Simone B <snb331@nyu.edu>
2021-05-21 14:33:37 -0400

    Implemented Joe's fixes

--- hw7_q6/snb331_hw7_q6.cpp
+++ hw7_q6/snb331_hw7_q6.cpp
@@ -1,29 +1,4 @@
 /*
- HI JOE! IF YOU ARE READING THIS, THANK YOU SO MUCH
-
- Basically what I have here is main1() and main2() that are called inside of main.
- The issue i'm having is with main1() when I'm creating an array of user input ints
- I added some cout statements to help me find the issue and this is what i got:
-
-inside function: 13.	 next line, outside function: 13.
-inside function: 13, 5.	 next line, outside function: 13, 5.
-inside function: 13, 5, 8.	 next line, outside function: 13, 5, 8.
-inside function: 13, 5, 8, 2.	 next line, outside function: 13, 5, 8, 2.
-inside function: 13, 5, 8, 2, 9.	 next line, outside function: 13, 5, 8, 2, 0.********* HERE'S THE ISSUE
-inside function: 13, 5, 8, 2, 0, 5.	 next line, outside function: 13, 5, 8, 2, 0, 5.
-inside function: 13, 5, 8, 2, 0, 5, 8.	 next line, outside function: 13, 5, 8, 2, 0, 5, 8.
-inside function: 13, 5, 8, 2, 0, 5, 8, 8.	 next line, outside function: 13, 5, 8, 2, 0, 5, 8, 8.
-
-before adding the 9, arrayPush_Back doubles the actual size of the array and then adds 9
-inside the arrayPush_Back this seems to happen fine
-But, on the next line outside of the function, the value gets changed to 0
-
-Any idea what is going on here?
-
- */
-
-
-/*
 Question 6:
 In this question, you will write two versions of a program that reads
 from the user a sequenceof positive integers ending with -1, and another
@@ -61,12 +36,12 @@ void printArray(int arr[], int arrSize);
 void printVector (vector <int> vect);
 void main1();
 void main2();
-void arrayPush_Back(int arr[], int &logicalArrSize, int &actualArrSize, int newInt);
-void arrayOfIntLines(int arr[], int arrSize, int searchVal, int outArray[], int &outArraySize);
+void arrayPush_Back(int* & arr, int &logicalArrSize, int &actualArrSize, int newInt);
+void arrayOfIntLines(int arr[], int arrSize, int searchVal, int * &outArray, int &outArraySize);
 vector <int> findIntIndices(vector <int> searchVector, int searchInt);
 int main() {
     main1();
-//    main2();
+    main2();
     return 0;
 }
 
@@ -81,9 +56,6 @@ void main1(){
     cin>>currentInt;
     while (currentInt >= 0 ){
         arrayPush_Back(uiArr, logicalUIArrSize, actualUIArrSize, currentInt);
-        cout<<"\t next line, outside function: ";
-        printArray(uiArr, logicalUIArrSize);
-        cout<<endl;
         cin>>currentInt;                                           // 13, 5, 8, 2, 9, 5, 8, 8
     }
 
@@ -97,13 +69,13 @@ void main1(){
         cout<<findInt<<" not found in sequence"<<endl;
     else{
         cout<<findInt<<" shows in line(s) ";
-        printArray(searchArr, logicalUIArrSize);                   // 2, 6.
+        printArray(searchArr, logicalSearchArrSize);                   // 2, 6.
         cout<<endl;
     }
     cout<<endl;
 }
 
-void arrayPush_Back(int arr[], int &logicalArrSize, int &actualArrSize, int newInt){
+void arrayPush_Back(int* &arr, int &logicalArrSize, int &actualArrSize, int newInt){
     int* tempArray;
 
     if (logicalArrSize == actualArrSize - 1){
@@ -116,21 +88,16 @@ void arrayPush_Back(int arr[], int &logicalArrSize, int &actualArrSize, int newI
     }
     arr[logicalArrSize] = newInt;
     logicalArrSize += 1;
-    cout<<"inside function: ";
-    printArray(arr, logicalArrSize);
 }
 
-void arrayOfIntLines(int arr[], int arrSize, int searchVal, int outArray[], int &outArraySize){
+void arrayOfIntLines(int arr[], int arrSize, int searchVal, int* &outArray, int &outArraySize){
     int lineNumber;
-    cout<<endl;
     for (int ind = 0; ind < arrSize; ind++) {
-        cout<<arr[ind]<<"\t";
         if (arr[ind] == searchVal){
             lineNumber = ind + 1;
             arrayPush_Back(outArray, outArraySize, arrSize, lineNumber);
         }
     }
-    cout<<endl;
 }
 
 void main2(){
